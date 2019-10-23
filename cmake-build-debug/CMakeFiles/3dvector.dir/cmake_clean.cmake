053d91cd560b1b1d069a48f086958a46ec5de5b6
Victor Phllippenko <phllippenko@inbox.ru>
2019-10-23 19:35:24 +0300

    Vector3D

---
+++ cmake-build-debug/CMakeFiles/3dvector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3dvector"
+  "3dvector.pdb"
+  "CMakeFiles/3dvector.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3dvector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
