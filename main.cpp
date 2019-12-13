90ad1c6c337a66a6c4b22b858f6a91cbc1244ead
GreenpeaceOne <philippenko@inbox.ru>
2019-12-13 20:48:30 +0300

    Abstract added on main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
 //    Vector3D a0;
 //    float *a0_t = a0.getValue();
 //    assert(*(a0_t+0) == 0 && *(a0_t+1)==0 && *(a0_t+1)==0);
-//
+// lalalalal
     Vector3D a(1.5, 2, 3);
 
 cout<<a;
